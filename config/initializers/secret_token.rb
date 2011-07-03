89112295ab087452b68e543ad0f12b13d547385d
Timur Valeev <timur.valeev@flatsoft.com>
2011-07-03 13:37:40 +0400

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ATeamPlanning::Application.config.secret_token = '5709d467b1ffa34bc7ebc8d6f8fd8405ecce38cccf3cce58ac01199b0c10cd37f594f2212b5252eafd2eb94030a60e97c60f44993b3824c4f9d03f7a6c5cd52c'
